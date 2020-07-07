8297660004419c4f83bf0239449ebfb80e9adfdc
Mimi Ojserkis <mimioj@gmail.com>
2020-07-07 22:11:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,13 @@
 
 
 def key_for_min_value(name_hash)
-
-  name_hash.each_key do |value|
-    puts value.min
+ smallest_value = 0
+ empty_hash = nil
+ name_hash.each do |name, value|
+  if smallest_value == 0 || value < smallest_value
+    smallest_value = value
+    empty_hash = name
   end
+ end
+ empty_hash
 end
